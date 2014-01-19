454d7f18dfc95ef22388466ff1691417a0cdb6db
anidev <anidev.aelico@gmail.com>
2014-01-18 19:56:06 -0500

    Added additional camera URL

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from verbose import *
 mjpgURL='http://10.6.12.11/axis-cgi/mjpg/video.cgi'
 mjpgURLdebug='http://127.0.0.1:8080/video.jpg'
 #mjpgURLdebug='http://watch.sniffdoghotel.com/axis-cgi/mjpg/video.cgi?resolution=320x240'
+#mjpgURLdebug='http://212.24.177.174:8092/axis-cgi/mjpg/video.cgi?resolution=320x240'
 ntIP='10.6.12.2'
 ntIPdebug='127.0.0.1'
 debug=False
